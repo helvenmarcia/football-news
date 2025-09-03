9582106fa12dadd0b05865bbfadd5ee2c2d057e5
Helvennn <enixgregory@gmail.com>
2025-09-03 14:46:14 +0700

    Context changed

--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
-        'class': 'PBP A'
+        'npm' : '2406359853',
+        'name': 'Helven Marcia',
+        'class': 'PBP C'
     }
 
     return render(request, "main.html", context)
