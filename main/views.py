8800c88922404f4d140327c7aa19a11644d644a9
Helven <helvenmarcia240306@gmail.com>
2025-09-24 15:57:34 +0700

    Tutorial 4: Desain Web & Metode Update dan Delete pada Data

--- main/views.py
+++ main/views.py
@@ -25,9 +25,9 @@ def show_main(request):
         news_list = News.objects.filter(user=request.user)
 
     context = {
-        'npm': '240123456',
+        'npm': '2406359853',
         'name': request.user.username,
-        'class': 'PBP A',
+        'class': 'PBP C',
         'news_list': news_list,
         'last_login': request.COOKIES.get('last_login', 'Never')
     }
@@ -47,6 +47,24 @@ def create_news(request):
     }
 
     return render(request, "create_news.html", context)
+
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context)
+
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
 
 @login_required(login_url='/login')
 def show_news(request, id):
